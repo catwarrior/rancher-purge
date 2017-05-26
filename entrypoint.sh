f6586712be2fba39d0f1d79f8afc9a213f3dd75d
catwarrior <lishufan00@aliyun.com>
2017-05-26 08:49:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,14 @@
 while :
 do
     echo "Checking for inactive hosts every $INTERVAL seconds..."
-    rancher hosts -a | grep -w inactive | while read LINE
+    rancher hosts -a | grep -E 'disconnected' | while read LINE1
+    do
+        HOST=$(echo $LINE1 | cut -d ' ' -f 1)
+        echo "stop $LINE1"
+        rancher stop --type host $HOST
+    done
+    sleep $INTERVAL
+    rancher hosts -a | grep -E 'inactive' | while read LINE
     do
         HOST=$(echo $LINE | cut -d ' ' -f 1)
         echo "Delete $LINE"
@@ -10,3 +17,4 @@ do
     done
     sleep $INTERVAL
 done
+
